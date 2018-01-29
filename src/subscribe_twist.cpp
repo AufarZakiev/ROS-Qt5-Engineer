58e652cd21a10a99b9a4a77d6700922a7b3743f8
AufarZakiev <zaufar@gmail.com>
2018-01-29 11:42:25 +0300

    Min linear speed feature added.

--- src/subscribe_twist.cpp
+++ src/subscribe_twist.cpp
@@ -4,24 +4,35 @@
 SubscribeTwist::SubscribeTwist() {
     //Topic you want to subscribe
     ROS_INFO("Constructor started!");
-    sub_ = n_.subscribe("/cmd_vel_mock", 1, &SubscribeTwist::callbackTwist, this);
+    sub_ = n_.subscribe("/cmd_vel", 1, &SubscribeTwist::callbackTwist, this);
     ROS_INFO("Subscribed!");
     //Robot object to send commands
     robot_ = new RobotSI();
     robot_->connectToEngineer();
-    ROS_INFO("Connecting? %d", robot_->isConnecting);
+    //ROS_INFO("Connecting? %d", robot_->isConnecting);
 }
 
 void SubscribeTwist::callbackTwist(const geometry_msgs::Twist &twist) {
-    ROS_INFO("In callback: connected? %d", robot_->isConnected);
-    ROS_INFO("In callback: connecting? %d", robot_->isConnecting);
-    robot_->moveD(twist.linear.x);
-    robot_->moveR(twist.angular.z);
-    ROS_INFO("Linear x: %f", twist.linear.x);
-    ROS_INFO("Angular z: %f", twist.angular.z);
+    float min_linear = 0.2;
+    if (fabs(twist.linear.x) < min_linear && twist.linear.x != 0.0) {
+        if (twist.linear.x > 0.0) {
+            ROS_INFO("Lin: %f.", min_linear);
+            robot_->moveD(min_linear);
+        } else {
+            ROS_INFO("Lin: %f.", -min_linear);
+            robot_->moveD(-min_linear);
+        }
+    } else {
+        robot_->moveD(twist.linear.x);
+        ROS_INFO("Lin: %f.", twist.linear.x);
+    }
+    robot_->moveR(-twist.angular.z);
+    ROS_INFO("Ang: %f", -twist.angular.z);
 }
 
 SubscribeTwist::~SubscribeTwist() {
+    robot_->moveD(0.0);
+    robot_->moveR(0.0);
     robot_->disconnectFromEngineer();
     delete (robot_);
 }
